5a189e4e89c408447d132aa41594554440f77a60
Harvey Ducay <ducs1007@gmail.com>
2024-06-20 11:23:03 +0800

    night commit

---
+++ api/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/')
+def home():
+    return jsonify({
+        'message': 'Hello World'
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
